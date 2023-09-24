0df0fb3a577b43ad3c83badcd9b5410478384826
wandersonxc <wandersonxcarvalho@gmail.com>
2023-09-24 15:09:35 -0300

    Projeto em reta final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,137 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
-const buttonDetail = document.getElementById('buttonDetail')
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const pokeballButtons = document.querySelectorAll('.pokeball-button');
+const pokemonDetails = document.getElementById('pokemonDetails');
+const closeButton = document.getElementById('closeButton');
+const pokemonImage = document.getElementById('pokemonImage');
+const pokemonName = document.getElementById('pokemonName');
+const pokemonNumber = document.getElementById('pokemonNumber');
+const pokemonTypes = document.getElementById('pokemonTypes');
+const pokemonAbilities = document.getElementById('pokemonAbilities');
+const pokemonStats = document.getElementById('pokemonStats');
 
 const maxRecords = 151;
-const limit = 10;
+const limit = 8;
 let offset = 0;
 
-
-function loadPokemonItens (offset,limit) {
-    pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type}">
-
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}" data-pokemon-details='${JSON.stringify(pokemon)}'>
             <div class="buttonDetail">
-                <button id="pokeballButton" class="pokeball-button">
+                <button class="pokeball-button">
                     <img src="pokeball.png" alt="Pokeball Icon">
-                </button>
+                </button>                
+            </div>     
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
-
-     
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-        
-                    <img src="${pokemon.photo}" 
-                                alt="${pokemon.name}">
-                </div>
-
-            </li>
-        `).join('')
-        pokemonList.innerHTML += newHtml
-    })
+        </li>
+    `;
 }
 
-loadPokemonItens(offset,limit)
+function loadPokemonItems(offset, limit) {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const promises = data.results.map((pokemon) => {
+                return fetch(pokemon.url)
+                    .then((response) => response.json())
+                    .then((pokemonData) => {
+                        const convertedPokemon = {
+                            number: pokemonData.id,
+                            name: pokemonData.name,
+                            type: pokemonData.types[0].type.name,
+                            types: pokemonData.types.map((typeData) => typeData.type.name),
+                            photo: pokemonData.sprites.front_default,
+                            abilities: pokemonData.abilities.map((abilityData) => abilityData.ability.name),
+                            stats: {}
+                        };
 
+                        for (const statData of pokemonData.stats) {
+                            convertedPokemon.stats[statData.stat.name] = statData.base_stat;
+                        }
+
+                        return convertedPokemon;
+                    });
+            });
+
+            Promise.all(promises)
+                .then((pokemons) => {
+                    const newHtml = pokemons.map(convertPokemonToLi).join('');
+                    pokemonList.innerHTML += newHtml;
+                    if (offset + limit >= maxRecords) {
+                        loadMoreButton.disabled = true;
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function showPokemonDetails(pokemon) {
+    pokemonImage.src = pokemon.photo;
+    pokemonName.textContent = pokemon.name;
+    pokemonNumber.textContent = `#${pokemon.number}`;
+    pokemonTypes.textContent = pokemon.types.join(', ');
+
+    pokemonAbilities.innerHTML = '';
+    pokemon.abilities.forEach((ability) => {
+        const li = document.createElement('li');
+        li.textContent = ability;
+        pokemonAbilities.appendChild(li);
+    });
+
+    pokemonStats.innerHTML = '';
+    for (const stat in pokemon.stats) {
+        const li = document.createElement('li');
+        li.textContent = `${stat}: ${pokemon.stats[stat]}`;
+        pokemonStats.appendChild(li);
+    }
+
+    pokemonDetails.style.display = 'block';
+}
+
+// Chame a função para carregar os Pokémon inicialmente
+loadPokemonItems(offset, limit);
+
+// Manipulador de eventos para o botão "Load More"
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordWithNextPage = offset + limit
+    offset += limit;
+    const qtdRecordWithNextPage = offset + limit;
 
     if (qtdRecordWithNextPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset,newLimit)
+        const newLimit = maxRecords - offset;
+        loadPokemonItems(offset, newLimit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+        // Remova o botão quando todos os Pokémon forem carregados
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset,limit)
+        loadPokemonItems(offset, limit);
     }
-})
+});
 
-function pokemonDetailClick(pokemon) {
-    console.log(loadPokemonDetails)
-    console.log(pokemon)
-}
+// Manipulador de eventos para o botão "Fechar" no card de detalhes
+closeButton.addEventListener('click', () => {
+    pokemonDetails.style.display = 'none';
+});
+
+// Adicione os manipuladores de eventos para os botões da Pokebola em cada Pokémon
+pokemonList.addEventListener('click', (event) => {
+    const pokeballButton = event.target.closest('.pokeball-button');
+    if (pokeballButton) {
+        const listItem = pokeballButton.closest('.pokemon');
+        const pokemon = JSON.parse(listItem.getAttribute('data-pokemon-details'));
+        if (pokemon) {
+            showPokemonDetails(pokemon);
+        }
+    }
+});
